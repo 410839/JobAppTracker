bcfc55818bff9aaf11850816a5e2297ff4cafd53
Connor Owens <cowens13@terpmail.umd.edu>
2026-02-23 14:58:47 -0500

    added services to jobs.services and a .env

--- frontend/src/services/jobService.js
+++ frontend/src/services/jobService.js
@@ -1,26 +1,54 @@
+const axios = require('axios');
 
+const API_BASE = process.env.REACT_APP_API_BASE_URL;
 
-const API_BASE = "give "
+const header_details = {'Content-Type': 'application/json; charset=UTF-8'};
 
-export async function getJobs(url) {
+async function perform_request(url, method_type, body_details=null) {
     try {
-        const response = await fetch(url)
-        if (!response.ok) {
-            throw new Error('HTTP error! status: ${response.status}');
+        let request_details = {method: method_type}
+        if (body_details != null) {
+            request_details.body = JSON.stringify(body_details);
+            request_details.headers = header_details;
         }
-    
+        const response = await fetch(url, request_details);
+        if (!response.ok) {
+            throw new Error(`HTTP error status: ${response.status}`);
+        }
         const data = await response.json();
         console.log(data);
-        return data
+        return data;
 
     } catch (error) {
         console.error('Fetch Error:', error);
-
     }
 }
 
-export async function createJobs(url) {
-    try {
-        const response = await fetch(url)
+export async function getJobs(page = null, status = null) {
+    let full_url = `${API_BASE}/jobs`;
+    if (page != null) {
+        
     }
+
+    
+    const data = await perform_request(full_url, "GET");
+    return data;
+}
+
+export async function createJob(jobData) {
+    const full_url = `${API_BASE}/jobs`;
+    const data = await perform_request(full_url, "POST", jobData);
+    return data;
+}
+
+export async function deleteJob(id) {
+    const full_url = `${API_BASE}/jobs/${id}`;
+    const data = await perform_request(full_url, "DELETE");
+    return data;
+}
+
+export async function patchJob(id, jobData) {
+    const full_url = `${API_BASE}/jobs/${id}`;
+    const data = await perform_request(full_url, "PATCH", jobData);
+    return data;
 }
