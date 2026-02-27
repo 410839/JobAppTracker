21b2ff7cb2877449b5d26330ce469fb27dc49302
Connor Owens <cowens13@terpmail.umd.edu>
2026-02-27 13:50:38 -0500

    added one route to app.js and fixed imports in index.js added simple test jobDetails page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import './styles/App.css';
-import getJobs from './services/jobService';
+import { BrowserRouter as Router,
+  Routes,
+  Route } from 'react-router-dom';
+import GetJobDetails from './pages/jobDetailsPage.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path = "/" element = {<GetJobDetails/>}> </Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/src/pages/jobDetailsPage.js
@@ -0,0 +1,8 @@
+import { useEffect, useState } from "react";
+
+
+export default function GetJobDetails() {
+    return (
+        <h1>GetJobDetails Page</h1>
+    );
+}
